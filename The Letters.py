09e4f601a4f2bc0482443f055b2edf032489b6e0
Jasmyn Oliver <42745370+Mizzjaze@users.noreply.github.com>
2018-10-05 11:40:27 -0400

    Update The Letters.py
    Cleaned up comments

--- The Letters.py
+++ The Letters.py
@@ -9,9 +9,8 @@ import pycorpora #imports pycorpora library
 import pronouncing #imports Pronouncing module
 
 
-#Defines function to select random types of 'passages' from pycorpora data set
+#Defines function to select random types of 'rooms' from pycorpora data set
 def get_entry():
-    #word_list = [pycorpora.architecture.passages['passages'],pycorpora.architecture.rooms['rooms']]
     ewords = []
     for entry in pycorpora.architecture.rooms['rooms']:
         plist = pronouncing.phones_for_word(entry)
@@ -21,11 +20,6 @@ def get_entry():
                 ewords.append(entry)
     return ewords
         
-##    entry = random.choice(pycorpora.architecture.passages['passages'])
-##    plist = pronouncing.phones_for_word(entry)
-##    scount = pronouncing.syllable_count(plist[0])
-##    return entry
-
 # Defines function to select random types of 'objects' from pycorpora data set
 def get_object():
     owords = []
@@ -35,9 +29,6 @@ def get_object():
             scount = pronouncing.syllable_count(plist[0])
             if (scount ==2):
                 owords.append(ranobject)
-##    ranobject = random.choice(pycorpora.objects.objects['objects'])
-##    plist = pronouncing.phones_for_word("tool")
-##    scount= pronouncing.syllable_count(plist[0])
     return owords
 
 def rhyme():
@@ -62,16 +53,6 @@ def main():
     template = "Where do we go? \nI don't have a {0} \nThe lake is cold \nWe need a {1}"
     pair = random.choice(rhyme())
     poem = template.format(pair[0],pair[1]) 
-##    poem = template.format(get_object(), get_entry())
-##    for i in range(4):
-##        low_rhyme = pronouncing.rhymes(get_object(),get_entry())
-##
-##        write_rhyme = random.choice(pronouncing.rhymes("write"))
-##
-##        print(poem.format(low_rhyme,write_rhyme))
-##
-##    poem = poem.format(get_object(), get_entry())
-##
     
     print(poem)
 
